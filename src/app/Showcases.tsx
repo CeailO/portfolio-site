8aaff57753ae0c19421f7ff7ead75059d33146b5
ceailo <s46390@student.umt.edu.my>
2023-02-06 02:58:09 +0800

    .

--- src/app/Showcases.tsx
+++ src/app/Showcases.tsx
@@ -15,7 +15,7 @@ export default function Showcases({}: Props) {
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Showcases
       </h3>
-      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80">
         {/* Project */}
         {projects.map((project, i) => (
           <div
