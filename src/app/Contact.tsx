88601e931a4f8f9d63182cf03b46a6f08bf467c6
ceailo <s46390@student.umt.edu.my>
2023-02-07 05:43:25 +0800

    .

--- src/app/Contact.tsx
+++ src/app/Contact.tsx
@@ -17,52 +17,72 @@ export default function Contact({}: Props) {
     window.location.href = `mailto:s46390@student.umt.edu.my?subject=${formData.subject}&body=Hi, my name is ${formData.name}. ${formData.message} (${formData.email})`;
   };
   return (
-    <div className="h-screen flex items-center">
+    <div className="h-screen flex flex-wrap items-center py-10 px-20">
+      <div className="space-y-10">
+        <h4 className="text-4xl lg:text-5xl font-semibold text-center">
+          Interested to collaborate?{" "}
+          <span className="decoration-[#f7ab0a]/50 underline">
+            Sent me inquiry
+          </span>
+        </h4>
+        <div className="space-y-4">
+          <div className="flex space-x-4 justify-center">
+            <SiTelegram className="text-[#f7ab0a] h-7 w-7 animate-pulse" />
+            <p className="text-xl">+60 11 6079 0057</p>
+          </div>
+          <div className="flex space-x-4 justify-center">
+            <SiDiscord className="text-[#f7ab0a] h-7 w-7 animate-pulse" />
+            <p className="text-xl">Barusu#5859</p>
+          </div>
+          <div className="flex space-x-4 justify-center">
+            <SiGmail className="text-[#f7ab0a] h-7 w-7 animate-pulse" />
+            <p className="text-xl">s46390@student.umt.edu.my</p>
+          </div>
+        </div>
+      </div>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col space-y-2 mx-auto"
+        className="flex flex-col mx-auto space-y-4"
       >
-        <div className="flex flex-col space-y-5">
-          <div className="flex flex-row space-x-4">
-            <input
-              {...register("name")}
-              className="contactInput"
-              type="text"
-              name="name"
-              id="name"
-              placeholder="Name"
-            />
-            <input
-              {...register("email")}
-              className="contactInput"
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Email"
-            />
-          </div>
+        <div className="flex flex-col lg:flex-row lg:space-x-4">
           <input
-            {...register("subject")}
+            {...register("name")}
             className="contactInput"
             type="text"
-            name="subject"
-            id="subject"
-            placeholder="Subject"
+            name="name"
+            id="name"
+            placeholder="Name"
           />
-          <textarea
-            {...register("message")}
+          <input
+            {...register("email")}
             className="contactInput"
-            name="message"
-            id="message"
-            placeholder="Message"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
           />
-          <button
-            type="submit"
-            className="bg-[#f7ab0a] py-5 text-black font-bold text-lg"
-          >
-            Submit
-          </button>
         </div>
+        <input
+          {...register("subject")}
+          className="contactInput"
+          type="text"
+          name="subject"
+          id="subject"
+          placeholder="Subject"
+        />
+        <textarea
+          {...register("message")}
+          className="contactInput"
+          name="message"
+          id="message"
+          placeholder="Message"
+        />
+        <button
+          type="submit"
+          className="bg-[#f7ab0a] py-5 text-black font-bold text-lg"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
