d891b8e669e2a5271d2c1c8580c31f87f7a4bd4c
ceailo <s46390@student.umt.edu.my>
2023-02-07 09:00:51 +0800

    .

--- src/app/Skills.tsx
+++ src/app/Skills.tsx
@@ -8,11 +8,11 @@ export default function Skills({}: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
+      className="h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
     >
       <h3
         id="hover-content"
-        className="lg:[writing-mode:vertical-lr] lg:rotate-180 top-36 break-words uppercase tracking-[3px] text-gray-500 text-sm"
+        className="px-10 py-10 lg:[writing-mode:vertical-lr] lg:rotate-180 top-36 break-words uppercase tracking-[3px] text-gray-500 text-sm"
       >
         Hover over a skill for currently proficiency
       </h3>
